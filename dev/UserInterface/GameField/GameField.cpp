f092840a94d435b7022021a7299ceb94abab260c
MishaNya0718 <misha_pisheta@mail.ru>
2022-07-17 03:14:04 +0500

    Исправлен косяк с полями

--- dev/UserInterface/GameField/GameField.cpp
+++ dev/UserInterface/GameField/GameField.cpp
@@ -34,9 +34,8 @@ void GameField::paintEvent(QPaintEvent *event) {
     QVector<char> character = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
     QVector<char> numbers = {'1', '2', '3', '4', '5', '6', '7', '8'};
 
-
     QPainter painter(this);
-    painter.setPen(QPen(Qt::red, 1, Qt::SolidLine, Qt::RoundCap));
+    painter.setPen(QPen(Qt::transparent, 1, Qt::SolidLine, Qt::RoundCap));
 
     //painter.drawRect(rectX, rectY, sizeBoard, sizeBoard);
 
@@ -45,65 +44,49 @@ void GameField::paintEvent(QPaintEvent *event) {
 
             if (i % 2 == 0) {
                 if (j % 2 == 0)
-                    painter.setBrush(QBrush(Qt::green));
+                    painter.setBrush(QColor(255, 218, 185));
                 else
-                    painter.setBrush(QBrush(Qt::darkYellow));
+                    painter.setBrush(QColor(160, 82, 45));
             }
             else {
                 if (j % 2 == 0)
-                    painter.setBrush(QBrush(Qt::darkYellow));
+                    painter.setBrush(QColor(160, 82, 45));
                 else
-                    painter.setBrush(QBrush(Qt::green));
+                    painter.setBrush(QColor(255, 218, 185));
             }
 
             QRect rect0(rectX + dx * i, rectY + dy * j, dx, dy);
+
+            painter.setPen(Qt::transparent);
             painter.drawRect(rect0);
+
+            //painter.setPen(Qt::black);
             //painter.drawText(rect0, Qt::AlignCenter, QString(character[i]));
         }
     }
 
-    painter.setBrush(QBrush(Qt::lightGray));
+    painter.setBrush(QColor(154,205,50));
     for (int i = 0; i < 8; i++) {
         // Верх
         painter.setPen(Qt::blue);
-        QRect rect1(rectX + dx * i, rectY, dx, -rectY + (rectY - dy)) ;
+        QRect rect1(rectX + dx * i, rectY, dx, -dy) ;
         painter.drawRect(rect1);
         painter.drawText(rect1, Qt::AlignCenter, QString(character[i]));
 
         // Низ
-        QRect rect2(rectX + dx * i, rectY + sizeBoard + dy, dx, -rectY + (rectY - dy));
+        QRect rect2(rectX + dx * i, rectY + sizeBoard + dy, dx, -dy);
         painter.drawRect(rect2);
         painter.drawText(rect2, Qt::AlignCenter, QString(character[i]));
 
         // Лево
-        QRect rect3(rectX - dx, rectY + dy + dy * i, dx, rectY - (rectY + dy));
+        QRect rect3(rectX - dx, rectY + dy * i, dx, dy);
         painter.drawRect(rect3);
         painter.drawText(rect3, Qt::AlignCenter, QString(numbers[7 - i]));
 
         // Право
-        QRect rect4(rectX + sizeBoard, rectY + dy + dy * i, dx, rectY - (rectY + dy));
+        QRect rect4(rectX + sizeBoard, rectY + dy * i, dx, dy);
         painter.drawRect(rect4);
         painter.drawText(rect4, Qt::AlignCenter, QString(numbers[7 - i]));
     }
 }
 
-
-/*
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            QPoint points[5] = {
-                QPoint(rectX + dx * i, rectY + dy * j),
-                QPoint(rectX + dx * (i + 1), rectY + dy * j),
-                QPoint(rectX + dx * (i + 1), rectY + dy * (j + 1)),
-                QPoint(rectX + dx * i, rectY + dy * (j + 1)),
-                QPoint(rectX + dx * i, rectY + dy * j)
-
-            };
-        if (i % 2 == 1)
-            painter.setBrush(QBrush(Qt::green));
-        else
-            painter.setBrush(QBrush(Qt::darkYellow));
-        painter.drawPolyline(points, 5);
-        }
-    }
-*/
